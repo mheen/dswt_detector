8bb5d20555c132cdbe494da2236b1ec6006a2c95
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2024-01-22 14:25:38 +0800

    Split load function, added new variables needed for DSWT detection

--- ocean_model_data.py
+++ ocean_model_data.py
@@ -8,6 +8,8 @@ from tools.roms import get_z, find_eta_xi_covering_lon_lat_box, convert_roms_u_v
 from tools.roms import get_eta_xi_along_transect, get_distance_along_transect
 from tools.seawater_density import calculate_density
 
+g = 9.81 # m/s2
+
 def select_input_files(input_dir:str, file_contains=None,
                        remove_gridfile=True, filetype='nc') -> list[str]:
     all_files = get_files_in_dir(input_dir, filetype)
@@ -23,8 +25,7 @@ def select_input_files(input_dir:str, file_contains=None,
         
     return files
 
-def load_roms_data(input_dir:str, grid_file=None, files_contain=None) -> xr.Dataset:
-    
+def read_roms_data(input_dir:str, grid_file:str, files_contain:str) -> xr.Dataset:
     paths = select_input_files(input_dir, file_contains=files_contain)
     roms_ds = xr.open_mfdataset(paths, data_vars='minimal')
     
@@ -48,27 +49,62 @@ def load_roms_data(input_dir:str, grid_file=None, files_contain=None) -> xr.Data
             roms_ds['hc'] = rg.hc
         if 'mask_rho' not in roms_ds.variables:
             roms_ds['mask_rho'] = rg.mask_rho
-    
-    # add layer depths z_rho and z_w
+            
+    return roms_ds
+
+def convert_roms_u_and_v(roms_ds:xr.Dataset) -> xr.Dataset:
+    # convert u and v to u_east and v_north
+    if 'u_eastward' not in roms_ds.variables:
+        u_eastward, v_northward = convert_roms_u_v_to_u_east_v_north(roms_ds.u.values, roms_ds.v.values, roms_ds.angle.values)
+        roms_ds['u_eastward'] = (['ocean_time', 's_rho', 'eta_rho', 'xi_rho'], u_eastward)
+        roms_ds['v_northward'] = (['ocean_time', 's_rho', 'eta_rho', 'xi_rho'], v_northward)
+        
+    return roms_ds
+            
+def add_variables_to_roms_data(roms_ds:xr.Dataset) -> xr.Dataset:
+    # --- calculate layer depths z_rho and z_w
     z_rho = get_z(roms_ds.Vtransform.values, roms_ds.s_rho.values, roms_ds.h.values, roms_ds.Cs_r.values, roms_ds.hc.values)
     roms_ds.coords['z_rho'] = (['s_rho', 'eta_rho', 'xi_rho'], z_rho)
     
     z_w = get_z(roms_ds.Vtransform.values, roms_ds.s_w.values, roms_ds.h.values, roms_ds.Cs_w.values, roms_ds.hc.values)
     roms_ds.coords['z_w'] = (['s_w', 'eta_rho', 'xi_rho'], z_w)
     
-    # convert u and v to u_east and v_north
-    if 'u_east' not in roms_ds.variables:
-        u_east, v_north = convert_roms_u_v_to_u_east_v_north(roms_ds.u.values, roms_ds.v.values, roms_ds.angle.values)
-        roms_ds['u_east'] = (['ocean_time', 's_rho', 'eta_rho', 'xi_rho'], u_east)
-        roms_ds['v_north'] = (['ocean_time', 's_rho', 'eta_rho', 'xi_rho'], v_north)
-
-    # calculate seawater density if temperature and salinity available
+    # --- calculate seawater density if temperature and salinity available
     if 'salt' in roms_ds.variables and 'temp' in roms_ds.variables:
         density = calculate_density(roms_ds.salt.values, roms_ds.temp.values, -roms_ds.z_rho.values)
         roms_ds['density'] = (['ocean_time', 's_rho', 'eta_rho', 'xi_rho'], density)
     else:
         missing_variable = [v for v in ['salt', 'temp'] if v not in roms_ds.variables]
         log.info(f'Cannot calculate seawater density, missing ROMS variable: {missing_variable}')
+    
+    if 'density' in roms_ds.variables:
+        # --- calculate depth mean density
+        delta_z_w = np.diff(roms_ds.z_w.values, axis=0)
+        depth_mean_density = np.sum(roms_ds.density.values*delta_z_w, axis=1)/roms_ds.h.values
+        roms_ds['depth_mean_density'] = (['ocean_time', 'eta_rho', 'xi_rho'], depth_mean_density)
+        
+        # --- calculate vertical density gradient
+        delta_z_rho = np.diff(roms_ds.z_rho.values, axis=0)
+        drhodz = -np.diff(roms_ds.density, axis=1)/delta_z_rho # minus diff(density) because 0 element is at bottom
+        # add dummy drhodz at surface so same number of s_rho-layers
+        # !!! FIX !!! better way to do this?
+        drhodz_resized = np.hstack((drhodz, np.expand_dims(drhodz[:, -1, :, :], 1))) # duplicating values along the surface
+        roms_ds['vertical_density_gradient'] = (['ocean_time', 's_rho', 'eta_rho', 'xi_rho'], drhodz_resized)
+        
+        # --- calculate potential energy anomaly
+        depth_mean_density_resized = np.repeat(depth_mean_density[:, np.newaxis, :, :], roms_ds.density.shape[1], axis=1)
+        phi = g/roms_ds.h.values*np.sum((depth_mean_density_resized-roms_ds.density.values)*roms_ds.z_rho.values*delta_z_w, axis=1)
+        roms_ds['potential_energy_anomaly'] = (['ocean_time', 'eta_rho', 'xi_rho'], phi)
+        
+    else:
+        log.info(f'Cannot calculate depth mean density, vertical density gradient, potential energy anomaly: missing density variable.')
+        
+    return roms_ds
+
+def load_roms_data(input_dir:str, grid_file=None, files_contain=None) -> xr.Dataset:
+    roms_ds = read_roms_data(input_dir, grid_file, files_contain)
+    roms_ds = convert_roms_u_and_v(roms_ds)
+    roms_ds = add_variables_to_roms_data(roms_ds)
 
     return roms_ds
 
@@ -104,3 +140,6 @@ def select_roms_transect(roms_ds:xr.Dataset,
     transect_ds.coords['distance'] = distance
     
     return transect_ds
+
+if __name__ == '__main__':
+    roms_ds = load_roms_data('tests/data/', 'tests/data/cwa_grid.nc', 'cwa_test_dswt')
