f97d156ef415f6198c794861dc0a3130661c9e2b
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2024-02-21 13:14:02 +0800

    Added option to add year range to read. Minor changes for newly downloaded dataset

--- read_climate_indices.py
+++ read_climate_indices.py
@@ -3,7 +3,7 @@ parent = os.path.abspath('.')
 sys.path.insert(1, parent)
 
 from tools.files import get_dir_from_json
-from tools.timeseries import convert_time_to_datetime, get_l_time_range, add_month_to_time
+from tools.timeseries import get_l_time_range, add_month_to_time
 import pandas as pd
 import xarray as xr
 from datetime import datetime, timedelta
@@ -11,7 +11,7 @@ import numpy as np
 
 # MEI: Multivariate El Nino Southern Oscillation Index
 # (from NOAA https://www.psl.noaa.gov/enso/mei)
-def read_mei_data(input_path=get_dir_from_json('mei'), skiprows=3) -> tuple[np.ndarray[datetime], np.ndarray[float]]:
+def read_mei_data(input_path=get_dir_from_json('mei'), skiprows=2, year_range=None) -> tuple[np.ndarray[datetime], np.ndarray[float]]:
     df = pd.read_csv(input_path, skiprows=skiprows)
     month_strs = ['DJ', 'JF', 'FM', 'MA', 'AM', 'MJ', 'JJ', 'JA', 'AS', 'SO', 'ON', 'ND']
 
@@ -23,15 +23,22 @@ def read_mei_data(input_path=get_dir_from_json('mei'), skiprows=3) -> tuple[np.n
         for m in range(1, 13):
             time.append(datetime(year, m, 15))
             mei.append(df_year[month_strs[m-1]].values[0])
+    time = np.array(time)
+    mei = np.array(mei)
+            
+    if year_range is not None:
+        l_time = get_l_time_range(time, datetime(year_range[0], 1, 1), datetime(year_range[1], 12, 31))
+        time = time[l_time]
+        mei = mei[l_time]
 
-    return np.array(time), np.array(mei)
+    return time, mei
 
 # DMI: Dipole Mode Index, indicator for the Indian Ocean Dipole
 # (from NOAA/ESRL: https://stateoftheocean.osmc.noaa.gov/sur/ind/dmi.php)
-def read_dmi_data(input_path=get_dir_from_json('dmi')) -> tuple[np.ndarray[datetime], np.ndarray[float]]:
+def read_dmi_data(input_path=get_dir_from_json('dmi'), year_range=None) -> tuple[np.ndarray[datetime], np.ndarray[float]]:
     
     ds = xr.load_dataset(input_path)
-    time = pd.to_datetime(ds['WEDCEN2'].values)
+    time = pd.to_datetime(ds['TIME'].values)
     dmi = ds.DMI.values
 
     dmi_monthly_mean = []
@@ -40,5 +47,12 @@ def read_dmi_data(input_path=get_dir_from_json('dmi')) -> tuple[np.ndarray[datet
     for t in time_monthly_mean:
         l_time = get_l_time_range(time, t, add_month_to_time(t, 1)-timedelta(days=1))
         dmi_monthly_mean.append(np.nanmean(dmi[l_time]))
+    time_monthly_mean = np.array(time_monthly_mean)
+    dmi_monthly_mean = np.array(dmi_monthly_mean)
 
-    return np.array(time_monthly_mean), np.array(dmi_monthly_mean)
+    if year_range is not None:
+        l_time = get_l_time_range(time_monthly_mean, datetime(year_range[0], 1, 1), datetime(year_range[1], 12, 31))
+        time_monthly_mean = time_monthly_mean[l_time]
+        dmi_monthly_mean = dmi_monthly_mean[l_time]
+
+    return time_monthly_mean, dmi_monthly_mean
