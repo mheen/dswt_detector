2752ec5606c1d7f3aecdb125d582eed7afb50739
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2024-03-22 16:46:27 +0800

    Added dswt_cross_shelf_transport_depth_range to config

--- tools/config.py
+++ tools/config.py
@@ -10,6 +10,7 @@ class Config:
     drhodz_depth_percentage: float
     minimum_percentage_consecutive_cells: float
     filter_depth: float
+    dswt_cross_shelf_transport_depth_range: float
     minimum_drhodz: float
 
 def read_config(model:str, input_path='input/configs/main_config.toml') -> dict:
@@ -22,5 +23,6 @@ def read_config(model:str, input_path='input/configs/main_config.toml') -> dict:
         drhodz_depth_percentage = config['drhodz_depth_percentage'],
         minimum_percentage_consecutive_cells = config['minimum_percentage_consecutive_cells'],
         filter_depth = config['filter_depth'],
+        dswt_cross_shelf_transport_depth_range = config['dswt_cross_shelf_transport_depth_range'],
         minimum_drhodz = config['minimum_drhodz']
     )
