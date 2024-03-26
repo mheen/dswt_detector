bc50a9e10bf9cef96d420d8b1ee96ab1688ba5e9
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2024-03-26 16:53:52 +0800

    Added config settings for cross-shelf transport

--- tools/config.py
+++ tools/config.py
@@ -12,6 +12,10 @@ class Config:
     filter_depth: float
     dswt_cross_shelf_transport_depth_range: float
     minimum_drhodz: float
+    cross_shelf_transport_depth: float
+    cross_shelf_bottom_layers_percentage: float
+    cross_shelf_surface_layers_percentage: float
+    cross_shelf_interior_layers_percentage: float
 
 def read_config(model:str, input_path='input/configs/main_config.toml') -> dict:
     full_config = toml.load(input_path)
@@ -25,10 +29,20 @@ def read_config(model:str, input_path='input/configs/main_config.toml') -> dict:
         dswt_cross_shelf_transport_depth_range = [dswt_cross_shelf_transport_depth_range[1],
                                                   dswt_cross_shelf_transport_depth_range[0]]
         
+    if config['cross_shelf_interior_layers_percentage'] is None:
+        config['cross_shelf_interior_layers_percentage'] = 1.0-config['cross_shelf_bottom_layers_percentage']-config['cross_shelf_surface_layers_percentage']
+    else: # check if sum of layers is 1, otherwise adjust interior layers to match
+        if config['cross_shelf_bottom_layers_percentage']+config['cross_shelf_surface_layers_percentage']+config['cross_shelf_interior_layers_percentage'] != 1.0:
+            config['cross_shelf_interior_layers_percentage'] = 1.0-config['cross_shelf_bottom_layers_percentage']-config['cross_shelf_surface_layers_percentage']
+ 
     return Config(
         drhodz_depth_percentage = config['drhodz_depth_percentage'],
         minimum_percentage_consecutive_cells = config['minimum_percentage_consecutive_cells'],
         filter_depth = config['filter_depth'],
         dswt_cross_shelf_transport_depth_range = dswt_cross_shelf_transport_depth_range,
-        minimum_drhodz = config['minimum_drhodz']
+        minimum_drhodz = config['minimum_drhodz'],
+        cross_shelf_transport_depth = config['cross_shelf_transport_depth'],
+        cross_shelf_bottom_layers_percentage = config['cross_shelf_bottom_layers_percentage'],
+        cross_shelf_surface_layers_percentage = config['cross_shelf_surface_layers_percentage'],
+        cross_shelf_interior_layers_percentage = config['cross_shelf_interior_layers_percentage']
     )
