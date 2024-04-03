5d1c976d917251debbd83c23d1bea78f48e7d01b
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2024-04-03 08:34:50 +0800

    If output file exists, now checking if it is complete and running from where it left off if not

--- main_dswt_detector.py
+++ main_dswt_detector.py
@@ -2,12 +2,13 @@ from readers.read_ocean_data import load_roms_data, select_roms_subset, select_i
 from transects import generate_transects_json_file, get_transects_dict_from_json, get_transects_in_lon_lat_range
 from dswt.dswt_detection import determine_daily_dswt_along_multiple_transects
 from dswt.cross_shelf_transport import calculate_daily_mean_cross_shelf_transport_at_depth_contour
+from tools.dswt_output import get_domain_str
 from tools.config import Config, read_config
 from tools import log
 from tools.files import get_dir_from_json, create_dir_if_does_not_exist
 import os
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 import xarray as xr
 from warnings import warn
@@ -16,8 +17,7 @@ from warnings import warn
 # User input
 # --------------------------------------------------------
 model = 'cwa'
-# years = np.arange(2000, 2024)
-years = [2017]
+years = np.arange(2000, 2024)
 
 # --- Domain range
 lon_range = [114.0, 116.0] # set to None for full domain
@@ -29,21 +29,15 @@ config = read_config(model)
 # --- Input file info
 model_input_dir = get_dir_from_json('cwa')
 grid_file = f'{model_input_dir}grid.nc' # set to None if grid information in output files
-files_contain = f'{model}_' # set to None if not needed
+file_preface = f'{model}_'
 
 # --------------------------------------------------------
 # Optional file settings (no need to change)
 transects_dir = 'input/transects/'
 create_dir_if_does_not_exist(transects_dir)
 
-# --- Automated domain name
-lon_range_str = f'{int(np.floor(lon_range[0]))}-{int(np.ceil(lon_range[1]))}'
-lon_range_unit = 'E' if lon_range[0] > 0 else 'W'
-lat_range_str = f'{int(abs(np.floor(lat_range[0])))}-{int(abs(np.ceil(lat_range[1])))}'
-lat_range_unit = 'S' if lat_range[0] < 0 else 'S'
-domain = f'{lon_range_str}{lon_range_unit}_{lat_range_str}{lat_range_unit}'
-
 # --- Output file info
+domain = get_domain_str(lon_range, lat_range)
 output_dir = f'output/{model}_{domain}/'
 create_dir_if_does_not_exist(output_dir)
 
@@ -71,7 +65,7 @@ else:
 # --------------------------------------------------------
 # check that files contain required variables (for 1 file)
 input_dir = f'{model_input_dir}{years[0]}/'
-roms_files = select_input_files(input_dir, files_contain)
+roms_files = select_input_files(input_dir, file_preface=file_preface)
 
 required_vars = ['ocean_time', 's_rho', 's_w',
                  'Vtransform', 'Cs_r', 'Cs_w', 'hc',
@@ -123,11 +117,21 @@ for year in years:
     output_dswt = f'{output_dir}dswt_{year}.csv'
     
     if os.path.exists(output_dswt):
-        log.info(f'Output already exists for {year}, skipping.')
-        continue
+        df_temp = pd.read_csv(output_dswt)
+        time = df_temp['time'].values
+        time_last = datetime.strptime(pd.unique(time)[-1], '%Y-%m-%d')
+        if time_last == datetime(year, 12, 31):
+            log.info(f'Output already exists for {year}, skipping.')
+            continue
+        else:
+            log.info(f'''Output partially exists for {year}. Running from {time_last+timedelta(days=1)} onwards.
+                     Please check to make sure that all transects were {time_last} were written to file.''')
+            date_range = [time_last+timedelta(days=1), datetime(year, 12, 31)]
+    else:
+        date_range = [datetime(year, 1, 1), datetime(year, 12, 31)]
     
     transects = get_transects_in_lon_lat_range(transects_file, lon_range, lat_range)
-    roms_files = select_input_files(input_dir, files_contain)
+    roms_files = select_input_files(input_dir, file_preface=file_preface, date_range=date_range)
     roms_files.sort()
 
     for file in roms_files:
@@ -144,6 +148,14 @@ for year in years:
         else:
             df_transects_dswt.to_csv(output_dswt, index=False)
 
+
+# --------------------------------------------------------
+# Output: timeseries and maps analyses and plots
+# --------------------------------------------------------
+
 # # to read csv and then get daily means:
 # df = pd.read_csv(output_dswt, index_col=['time', 'transect']) # (this is a MultiIndex DataFrame)
 # df.groupby('time').mean()
+
+# # to select specific values:
+# df.xs('2017-01-01', level='time', drop_level=False).xs('t153', level='transect', drop_level=False)
