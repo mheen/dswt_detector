Fixed major bugs in transport calculation

dswt/cross_shelf_transport.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
 from scipy.ndimage import gaussian_filter
+from warnings import warn
 
 def get_angle_between_mask_and_velocity(mask:np.ndarray[bool],
                                         u:np.ndarray[float],
@@ -99,13 +100,17 @@ def calculate_dswt_cross_shelf_transport_along_transect(
     l_dswt:np.array,
     config:Config) -> tuple[np.ndarray, np.ndarray]:
 
-    if np.all(l_dswt) == False:
-        return [0.0], [0.0], [np.nan], [np.nan], [np.nan]
+    if np.all(l_dswt == False):
+        return [np.nan], [np.nan], [np.nan], [np.nan], [np.nan], [np.nan], [np.nan]
     
     transect_ds = add_down_transect_velocity_to_ds(transect_ds)
 
     transport = np.empty(len(transect_ds.distance))*np.nan
     mean_vel = np.empty(len(transect_ds.distance))*np.nan
+    mean_dz = np.empty(len(transect_ds.distance))*np.nan
+    sum_dz = np.empty(len(transect_ds.distance))*np.nan
+    sum_vel = np.zeros(len(transect_ds.distance))
+    vel_counts = np.zeros(len(transect_ds.distance))
     
     i_dswt = np.where(l_dswt == True)[0] # times where there is DSWT
 
@@ -124,15 +129,35 @@ def calculate_dswt_cross_shelf_transport_along_transect(
                 continue # drho/dz condition not satisfied at this location along shelf
             
             # calculate transport over all depth layers below first spike in drho/dz
-            k = i_depth[-1]
-            # mean with already existing values at locations
-            transport[j] = np.nanmean([transport[j], np.nansum(transect_ds.down_transect_vel.values[i, 0:k, j]*transect_ds.delta_z.values[0:k, j])*transect_ds.dt.values])
-            mean_vel[j] = np.nanmean([mean_vel[j], np.nanmean(transect_ds.down_transect_vel.values[i, 0:k, j])])
+            if i_depth[-1] == 0:
+                k = 0
+            else:
+                k = i_depth[-1]-1
+            # sum with already existing values at locations
+            if np.all(np.isnan(transect_ds.down_transect_vel.values[i, 0:k, j])):
+                # FIX! there seem to be some grid cells close to land that do not have a velocity but do have other parameters
+                # this probably needs a fix in the conversion of u and v to rho points
+                # I am skipping these cells for now, as these should be limited and only cells close to land
+                continue
+            if np.logical_and(i == 3, j == 8) == True:
+                print('bla')
+            transport[j] = np.nansum([transport[j], np.nansum(transect_ds.down_transect_vel.values[i, 0:k, j]*transect_ds.delta_z.values[0:k, j])*transect_ds.dt.values])
+            depth_mean_vel = np.nansum(transect_ds.down_transect_vel.values[i, 0:k, j]*transect_ds.delta_z.values[0:k, j])/np.nansum(transect_ds.delta_z.values[0:k, j])
+            sum_vel[j] = np.nansum([sum_vel[j], depth_mean_vel])
+            vel_counts[j] += 1
+            sum_dz[j] = np.nansum([sum_dz[j], np.nansum(transect_ds.delta_z.values[0:k, j])])
+            
+    mean_vel = sum_vel/vel_counts
+    mean_dz = sum_dz/vel_counts
     
     l_nonzero = np.logical_and(np.logical_and(~np.isnan(transport), transport != 0.0),
-                              np.logical_and(~np.isnan(mean_vel), mean_vel != 0.0))
+                               np.logical_and(~np.isnan(mean_vel), mean_vel != 0.0))
     
-    if np.sum(l_nonzero) == 0:
-        return [0.0], [0.0], [np.nan], [np.nan], [np.nan]
+    if np.sum(l_nonzero) == 0: # you shouldn't really end up here...
+        warn(f'DSWT identified in transect, but no cross-shelf transport found, returning NaNs.')
+        return [np.nan], [np.nan], [np.nan], [np.nan], [np.nan], [np.nan], [np.nan]
     
-    return transport[l_nonzero], mean_vel[l_nonzero], transect_ds.lon_rho.values[l_nonzero], transect_ds.lat_rho.values[l_nonzero], transect_ds.h.values[l_nonzero]
+    return (transport[l_nonzero], mean_vel[l_nonzero], mean_dz[l_nonzero],
+            transect_ds.distance.values[l_nonzero],
+            transect_ds.lon_rho.values[l_nonzero], transect_ds.lat_rho.values[l_nonzero],
+            transect_ds.h.values[l_nonzero])

dswt/dswt_detection.py
@@ -57,7 +57,7 @@ def determine_dswt_along_transect(transect_ds:xr.Dataset, config:Config):
     
     # remove data for depths above filter_depth_up_to
     if config.filter_depth is not None:
-        # mask (replace with NaNs) all values where depth > filter_depth_up_to:
+        # mask (replace with NaNs) all values where depth > filter_depth:
         transect_ds = transect_ds.where(transect_ds.h < config.filter_depth)
     
     # condition 1: horizontal density gradient (away from coast) must be negative
@@ -96,20 +96,22 @@ def determine_daily_dswt_along_multiple_transects(roms_ds:xr.Dataset, transects:
     transect_names = list(transects.keys())
     
     df_transects_dswt = pd.DataFrame(index=np.arange(0, len(transect_names)),
-                                     columns=['time', 'transect', 'f_dswt', 'vel_dswt', 'transport_dswt', 'lon_transport', 'lat_transport', 'depth_transport'])
+                                     columns=['time', 'transect', 'f_dswt', 'vel_dswt', 'transport_dswt', 'dz_dswt', 'ds', 'lon_transport', 'lat_transport', 'depth_transport'])
     time = pd.to_datetime(roms_ds.ocean_time.values[0]).date()
     row = 0
-    for i, transect_name in enumerate(transect_names):
+    # for i, transect_name in enumerate(transect_names):
+    for i, transect_name in enumerate(['t153']):
         eta = transects[transect_name]['eta']
         xi = transects[transect_name]['xi']
         
         transect_ds = select_roms_transect_from_known_coordinates(roms_ds, eta, xi)
         l_dswt, _, _, _, _ = determine_dswt_along_transect(transect_ds, config)
         
-        dswt_cross_transport, dswt_cross_vel, lon, lat, h = calculate_dswt_cross_shelf_transport_along_transect(transect_ds, l_dswt, config)
+        dswt_cross_transport, dswt_cross_vel, dswt_dz, ds, lon, lat, h = calculate_dswt_cross_shelf_transport_along_transect(transect_ds, l_dswt, config)
         
         for j in range(len(dswt_cross_transport)):
-            df_transects_dswt.loc[row] = [time, transect_name, np.nanmean(l_dswt.astype(int)), dswt_cross_vel[j], dswt_cross_transport[j], lon[j], lat[j], h[j]]
+            df_transects_dswt.loc[row] = [time, transect_name, np.nanmean(l_dswt.astype(int)),
+                                          dswt_cross_vel[j], dswt_cross_transport[j], ds[j], dswt_dz[j], lon[j], lat[j], h[j]]
             row += 1
         
     return df_transects_dswt
@@ -118,7 +120,7 @@ if __name__ == '__main__':
     output_dswt = 'output/cwa_114-116E_33-31S/dswt_2017.csv'
     
     model_input_dir = get_dir_from_json('cwa')
-    files = ['cwa_20170603_03__his.nc', 'cwa_20170101_00__his.nc']
+    files = ['cwa_20170420_03__his.nc']
     grid_file = f'{model_input_dir}grid.nc'
     
     lon_range = [114., 116.]