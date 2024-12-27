ec46bce0cd18596a97a1a7163d3236fe1cd24896
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2024-12-27 14:24:35 +0800

    Changed tests

--- tests/test_dswt_detection.py
+++ tests/test_dswt_detection.py
@@ -3,39 +3,26 @@ parent = os.path.abspath('.')
 sys.path.insert(1, parent)
 
 import pytest
-from readers.read_ocean_data import load_roms_data, select_roms_transect
+from tools.config import read_config
+from transects import read_transects_dict_from_json
+from readers.read_ocean_data import load_roms_data, select_roms_transect_from_known_coordinates
 from dswt.dswt_detection import determine_dswt_along_transect
 
-transect_coords1 = [115.7, -31.76, 115.26, -31.95]
+transects_file = f'input/transects/test_transects.json'
+transects = read_transects_dict_from_json(transects_file)
 
-def test_cwa_roms_20170613():
-    roms_ds = load_roms_data('tests/data/', grid_file='tests/data/cwa_grid.nc', files_contain='cwa_20170613')
-    transect_ds = select_roms_transect(roms_ds, transect_coords1[0], transect_coords1[1],
-                                       transect_coords1[2], transect_coords1[3])
-    l_dswt = determine_dswt_along_transect(transect_ds, minimum_drhodz=0.02, minimum_p_cells=0.3, filter_depth=100.)
+config = read_config('test')
+
+ds_roms = load_roms_data('tests/data/2017/test_20170607_03__his.nc', grid_file='tests/data/grid.nc')
+
+transect_name = 't162'
+
+eta = transects[transect_name]['eta']
+xi = transects[transect_name]['xi']
+
+transect_ds = select_roms_transect_from_known_coordinates(ds_roms, eta, xi)
+
+def test_detection():
+    l_dswt, _, _, _, _ = determine_dswt_along_transect(transect_ds, config)
     l_dswt0 = [True, True, True, True, True, True, True, True]
-    assert (l_dswt == l_dswt0).all()
-    
-def test_cwa_roms_20170623():
-    roms_ds = load_roms_data('tests/data/', grid_file='tests/data/cwa_grid.nc', files_contain='cwa_20170623')
-    transect_ds = select_roms_transect(roms_ds, transect_coords1[0], transect_coords1[1],
-                                       transect_coords1[2], transect_coords1[3])
-    l_dswt = determine_dswt_along_transect(transect_ds, minimum_drhodz=0.02, minimum_p_cells=0.3, filter_depth=100.)
-    l_dswt0 = [False, False, False, False, False, False, False, False]
-    assert (l_dswt == l_dswt0).all()
-    
-def test_ozroms_20170613():
-    roms_ds = load_roms_data('tests/data/', files_contain='ozroms_20170613')
-    transect_ds = select_roms_transect(roms_ds, transect_coords1[0], transect_coords1[1],
-                                       transect_coords1[2], transect_coords1[3])
-    l_dswt = determine_dswt_along_transect(transect_ds, minimum_drhodz=0.01, minimum_p_cells=0.3, filter_depth=100.)
-    l_dswt0 = [True]
-    assert (l_dswt == l_dswt0).all()
-    
-def test_ozroms_20170623():
-    roms_ds = load_roms_data('tests/data/', files_contain='ozroms_20170623')
-    transect_ds = select_roms_transect(roms_ds, transect_coords1[0], transect_coords1[1],
-                                       transect_coords1[2], transect_coords1[3])
-    l_dswt = determine_dswt_along_transect(transect_ds, minimum_drhodz=0.01, minimum_p_cells=0.3, filter_depth=100.)
-    l_dswt0 = [False]
     assert (l_dswt == l_dswt0).all()
--- tests/test_roms.py
+++ tests/test_roms.py
@@ -7,48 +7,29 @@ import xarray as xr
 import numpy as np
 from tools.roms import get_eta_xi_of_lon_lat_point, find_eta_xi_covering_lon_lat_box, get_eta_xi_along_transect
 from tools.roms import get_z
-from readers.read_ocean_data import select_input_files, load_roms_data
+from readers.read_ocean_data import load_roms_data
 
-cwa_grid = xr.load_dataset('tests/data/cwa_grid.nc')
-cwa_lon = cwa_grid.lon_rho.values
-cwa_lat = cwa_grid.lat_rho.values
+grid_file = 'tests/data/grid.nc'
+ds_grid = xr.load_dataset(grid_file)
+lon_grid = ds_grid.lon_rho.values
+lat_grid = ds_grid.lat_rho.values
 
-cwa_ds = load_roms_data('tests/data/', files_contain='cwa_20170613', grid_file='tests/data/cwa_grid.nc')
+ds_roms = load_roms_data('tests/data/2017/test_20170607_03__his.nc', grid_file=grid_file)
 
-class TestCoords:
-    def test_point(self):
-        eta, xi = get_eta_xi_of_lon_lat_point(cwa_lon, cwa_lat, 115.70, -31.76)
-        assert (eta, xi) == (477, 111)
-        
-    def test_box(self):
-        xi0, xi1, eta0, eta1 = find_eta_xi_covering_lon_lat_box(cwa_lon, cwa_lat, [114.0, 116.0], [-32.0, -30.0])
-        assert (xi0, xi1, eta0, eta1) == (368, 479, 100, 208)
-        
-    def test_transect(self):
-        etas, xis = get_eta_xi_along_transect(cwa_lon, cwa_lat, 115.7, -31.76, 115.26, -31.95, 500.)
-        etas0 = np.array([111, 111, 111, 111, 111, 110, 110, 110, 110, 110, 110, 109, 109,
-                          109, 109, 109, 109, 108, 108, 108, 108, 108, 108, 107, 107, 107,
-                          107, 107, 107, 106, 106, 106, 106, 106, 106, 105, 105])
-        xis0 = np.array([477, 476, 475, 474, 473, 473, 472, 471, 470, 469, 468, 468, 467,
-                         466, 465, 464, 463, 463, 462, 461, 460, 459, 458, 458, 457, 456,
-                         455, 454, 453, 453, 452, 451, 450, 449, 448, 448, 447])
-        assert (etas == etas0).all() & (xis == xis0).all()
+def test_point():
+    eta, xi = get_eta_xi_of_lon_lat_point(lon_grid, lat_grid, 115.70, -31.76)
+    assert (eta, xi) == (151, 73)
+    
+def test_box():
+    xi0, xi1, eta0, eta1 = find_eta_xi_covering_lon_lat_box(lon_grid, lat_grid, [114.0, 116.0], [-32.0, -30.0])
+    assert (xi0, xi1, eta0, eta1) == (42, 152, 62, 124)
         
 def test_z_rho():
-    z_rho = get_z(cwa_grid.Vtransform.values, cwa_grid.s_rho.values, cwa_grid.h.values, cwa_grid.Cs_r.values, cwa_grid.hc.values)
-    assert np.round(z_rho[0, 0, 0], 2) == -4901.25 and np.round(z_rho[-1, 320, 240], 2) == -3.97
-    
-class TestLoadRoms:    
-    def test_input_file_selection(self):
-        paths = select_input_files('tests/data/', file_contains='cwa_20170613')
-        assert paths == ['tests/data/cwa_20170613.nc']
+    z_rho = get_z(ds_grid.Vtransform.values, ds_grid.s_rho.values, ds_grid.h.values, ds_grid.Cs_r.values, ds_grid.hc.values)
+    assert np.round(z_rho[0, 0, 0], 2) == -2960.9
 
-    def test_grid_file_error(self):
-        with pytest.raises(ValueError):
-            load_roms_data('tests/data/', files_contain='cwa_20170613')
-            
 def test_u_east_conversion():
-    assert np.round(cwa_ds.u_eastward[0, 0, 0, 0].values, 3) == -0.065 and np.round(cwa_ds.u_eastward[4, 10, 320, 240].values, 3) == -0.045
+    assert np.round(ds_roms.u_eastward[0, 0, 0, 0].values, 3) == -0.031
     
 def test_v_north_conversion():
-    assert np.round(cwa_ds.v_northward[0, 0, 0, 0].values, 3) == -0.025 and np.round(cwa_ds.v_northward[4, 10, 320, 240].values, 3) == 0.062
+    assert np.round(ds_roms.v_northward[0, 0, 0, 0].values, 3) == 0.132
