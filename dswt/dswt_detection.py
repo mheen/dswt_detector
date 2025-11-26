85e9b476e7cea4dadb8f021f1538ced16b423b37
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2025-11-26 11:42:54 +0800

    Fixed major bug in returning detection output

--- dswt/dswt_detection.py
+++ dswt/dswt_detection.py
@@ -3,100 +3,113 @@ parent = os.path.abspath('.')
 sys.path.insert(1, parent)
 
 from tools.files import get_dir_from_json
-from tools.config import read_config
+from tools.config import read_config, Config
 from transects import read_transects_in_lon_lat_range_from_json
 from readers.read_ocean_data import load_roms_data
 
 from readers.read_ocean_data import select_roms_transect_from_known_coordinates
 from dswt.cross_shelf_transport import calculate_dswt_cross_shelf_transport_along_transect
-from tools.config import Config
 from itertools import groupby
 import numpy as np
 import xarray as xr
 import pandas as pd
 
-def calculate_horizontal_density_gradient_along_transect(transect_ds:xr.Dataset) -> float:
-    drho = np.diff(transect_ds.depth_mean_density.values, axis=1)
-    dx = np.diff(transect_ds.distance.values)
-    
-    drhodx = drho/dx
-    
-    # add dummy value at end for size consistency
-    # !!! FIX !!! find a better way to do this
-    drhodx = np.hstack((drhodx, np.expand_dims(drhodx[:, -1], 1)))
+RHO0 = 1025.
 
-    return drhodx
-
-def determine_dswt_along_transect(transect_ds:xr.Dataset, config:Config):
-    '''Determines if DSWT is occurring along a transect or not.
-    Conditions for DSWT are:
-    1. There must be a negative horizontal density gradient along the transect
-       (higher depth mean density at the coast, decreasing density towards open ocean).
-       This is determined by taking the mean horizontal density gradient over the entire
-       transect, and so returns one bool per time.
-       The default value for minimum_drhodz = 0.02, but this needs to be chosed with care
-       depending on the ocean model used. The "plot_cycling_..." functions in this script
-       can be used to help determine what this value should be.
-    2. The vertical density gradient must exceed a minimum value (minimum_drhodz) for
-       at least a minimum amount of consecutive grid cells (determined as a percentage
-       of the total number of cells along the transect: minimum_p_cells). This must happen
-       in the bottom layers of the model, specified as a percentage of cells (drhodz_depth_p).
-       This condition also returns one bool per time over the entire transect.
-       The default value for minimum_p_cells = 0.3 (30%), which should hopefully work
-       independent of the ocean model used.
-       The default value for drhodz_depth_p = 0.5 (50%), which should hopefully work
-       independent of the ocean model used. This condition was added because there
-       can sometimes be high drhodz values at the ocean surface (for example when
-       there are high salinity values at the ocean surface and when temperature values
-       do not change much).
-    
-    The depth up to which data along the transect is used can be filtered by setting a maximum
-    depth value for filter_depth. The default value for filter_depth = 100.0 (m). This filter
-    option can be useful to prevent colder deep water (off the shelf) from showing up as DSWT.
-    This option may be redundant with the drho/dz condition used though.'''
+def determine_dswt_along_transect(transect_ds:xr.Dataset, config:Config, mld_condition=True):
+    '''New method:
+    1. MLD at coast must reach bottom (NOTE: think about how to implement this for island transects)
+    2. drho/dx < 0
+    3. drho * s / rho0 < -2 10**-8
+    4. transport in the bottom layer must be offshore'''
     
     # remove data for depths above filter_depth_up_to
     if config.filter_depth is not None:
-        # mask (replace with NaNs) all values where depth > filter_depth:
-        transect_ds = transect_ds.where(transect_ds.h < config.filter_depth)
+        # replace with NaNs all values where depth > filter_depth:
+        l_depth = transect_ds.h.values < config.filter_depth
+        # transect_ds.slope.values[~l_depth] = np.nan
+        transect_ds.drho_z.values[:, :, ~l_depth] = np.nan
+        transect_ds.drho_dx_zmean.values[:, ~l_depth] = np.nan
+        transect_ds.u_down.values[:, :, ~l_depth] = np.nan
     
-    # condition 1: horizontal density gradient (away from coast) must be negative
-    # this is determined per time over the entire transect
-    drhodx = calculate_horizontal_density_gradient_along_transect(transect_ds)
-    condition1 = np.nanmean(drhodx, axis=1) < 0. # taking mean over entire transect
+    # condition 1: mixed layer depth must reach the seafloor at the coast
+    if mld_condition == True:
+        i_coast = np.where(~np.isnan(transect_ds.density.values))[2][0]
+        drho = transect_ds.density[:, -1, i_coast] - transect_ds.density[:, 0, i_coast] # s_rho[0] = bottom, s_rho[-1] = surface
+        mld_condition = abs(drho) / RHO0 < 10**-4 # mld_condition : [ocean_time]
+    else:
+        mld_condition = np.ones(len(transect_ds.ocean_time)).astype(bool)
     
-    # condition 2: vertical density gradient > a minimum value (differs per ocean model)
-    # AND the vertical density gradient must exceed this value for a minimum number of cells
-    # the result is that DSWT is determined for the entire transect
-    n_z_layers = len(transect_ds.s_rho)
-    n_depth_layers = int(np.ceil(n_z_layers*config.drhodz_depth_percentage))
-    l_drhodz = np.any(transect_ds.vertical_density_gradient[:, 0:n_depth_layers, :] > config.minimum_drhodz, axis=1) # [time, distance] (check for any along depth)
-    n_used_cells = sum(~np.isnan(transect_ds.h.values))
-    drhodz_max = []
-    drhodz_cells = []
-    condition2 = []
-    for t in range(len(transect_ds.ocean_time)):
-        consecutive_true_lengths = [len(list(v)) for k, v in groupby(l_drhodz.values[t, :]) if k == True]
-        if len(consecutive_true_lengths) != 0:
-            n_consecutive_cells = max(consecutive_true_lengths)
-        else:
-            n_consecutive_cells = 0
-        drhodz_cells.append(n_consecutive_cells/n_used_cells)
-        drhodz_max.append(np.nanmax(transect_ds.vertical_density_gradient[t, 0:n_depth_layers, :]))
-        condition2.append((n_consecutive_cells/n_used_cells) >= config.minimum_percentage_consecutive_cells)
-    condition2 = np.array(condition2)
+    # condition 2: mean depth mean horizontal density gradient (away from coast) must be negative
+    mean_drhodx = np.nanmean(transect_ds.drho_dx_zmean.values, axis=1)
+    drhodx_condition = mean_drhodx < 0. # drhodx_condition: [ocean_time]
     
-    # DSWT along a transect when both condition 1 and condition 2 hold
-    l_dswt = np.logical_and(condition1, condition2)
+    l_time_dswt_possible = np.logical_and(mld_condition, drhodx_condition)
     
-    return l_dswt, condition1, condition2, drhodz_max, drhodz_cells
+    if np.any(l_time_dswt_possible) == True:
+        # condition 3: vertical density gradient needs to be sufficiently large
+        drho_s = (transect_ds.drho_z.values * transect_ds.slope.values) / RHO0
+        drho_s_condition = drho_s < -2*10**-8 # drho_s_condition: [ocean_time, s_rho, distance]
+        # consider only vertical density gradient at certain depth (remove any True values from surface layers)
+        minimum_depth = transect_ds.h.values - config.drhodz_depth_percentage * transect_ds.h.values # note: h is positive
+        l_shallow = transect_ds.z_rho.values > -minimum_depth # note: z_rho is negative, minimum depth is positive
+        drho_s_condition[:, l_shallow] = False
+        
+        # condition 4: down slope velocity needs to be positive (offshore/down-slope)
+        u_down_condition = transect_ds.u_down.values > 0. # u_down_condition: [ocean_time, s_rho, distance]
+        
+        l_dswt = np.logical_and(drho_s_condition[l_time_dswt_possible, :, :], u_down_condition[l_time_dswt_possible, :, :])
+        
+        all_transport = transect_ds.u_down.values * transect_ds.delta_z.values * transect_ds.dt.values
+        
+        t_dswt = np.unique(np.where(l_dswt == True)[0])
+        if len(t_dswt) == 0:
+            return (t_dswt, np.array([np.nan]), np.array([np.nan]), np.array([np.nan]), np.array([np.nan]), np.array([np.nan]), np.array([np.nan]),
+            np.array([np.nan]), np.nan, np.nan, np.array([np.nan]), np.array([np.nan]))
+        
+        transport_dswt = np.zeros((len(transect_ds.ocean_time), len(transect_ds.distance)))
+        depth_mean_vel_dswt = np.zeros((len(transect_ds.ocean_time), len(transect_ds.distance)))
+        thickness_dswt = np.zeros((len(transect_ds.ocean_time), len(transect_ds.distance)))
+        min_drho_s = np.zeros((len(transect_ds.ocean_time), len(transect_ds.distance)))
+        mean_drho_s = np.zeros((len(transect_ds.ocean_time), len(transect_ds.distance)))
+        for t in t_dswt:
+            x_dswt = np.where(np.any(l_dswt[t, :, :], axis=0))[0]
+            for x in x_dswt:
+                z_dswt = np.where(l_dswt[t, :, x] == True)[0][-1] # shallowest layer up to which DSWT extends
+                # calculate transport from bottom up to shallowest layer where there is DSWT
+                transport_dswt[t, x] = np.nansum(all_transport[t, 0:z_dswt + 1, x] * u_down_condition[t, 0:z_dswt + 1, x].astype(int))
+                thickness_dswt[t, x] = np.nansum(transect_ds.delta_z.values[0:z_dswt + 1, x])
+                depth_mean_vel_dswt[t, x] = transport_dswt[t, x] / (transect_ds.dt.values * thickness_dswt[t, x])
+                min_drho_s[t, x] = np.nanmin(drho_s[t, 0:z_dswt + 1, x] * u_down_condition[t, 0:z_dswt + 1, x].astype(int))
+                mean_drho_s[t, x] = np.nanmean(drho_s[t, 0:z_dswt + 1, x] * u_down_condition[t, 0:z_dswt + 1, x].astype(int))
+                
+        daily_transport_dswt = np.nansum(transport_dswt, axis=0)
+        daily_mean_thickness_dswt = np.nanmean(thickness_dswt, axis=0)
+        daily_mean_vel_dswt = np.nanmean(depth_mean_vel_dswt, axis=0)
+        daily_min_drho_s = np.nanmin(min_drho_s, axis=0)
+        daily_mean_drho_s = np.nanmean(mean_drho_s, axis=0)
+        daily_mean_drhodx = np.nanmean(mean_drhodx[t_dswt])
+        daily_min_drhodx = np.nanmin(mean_drhodx[t_dswt])
+        
+        x_dswt_all = np.where(daily_transport_dswt != 0)[0]
+        
+        return (t_dswt, daily_mean_vel_dswt[x_dswt_all], daily_mean_thickness_dswt[x_dswt_all], daily_transport_dswt[x_dswt_all],
+                transect_ds.distance.values[x_dswt_all], transect_ds.lon_rho.values[x_dswt_all], transect_ds.lat_rho.values[x_dswt_all],
+                transect_ds.h.values[x_dswt_all], daily_mean_drhodx, daily_min_drhodx, daily_mean_drho_s[x_dswt_all], daily_min_drho_s[x_dswt_all])
+        
+    return (np.array([]), np.array([np.nan]), np.array([np.nan]), np.array([np.nan]), np.array([np.nan]), np.array([np.nan]), np.array([np.nan]),
+            np.array([np.nan]), np.nan, np.nan, np.array([np.nan]), np.array([np.nan]))
+
 
 def determine_daily_dswt_along_multiple_transects(roms_ds:xr.Dataset, transects:dict, config:Config) -> pd.DataFrame:
     
     transect_names = list(transects.keys())
     
     df_transects_dswt = pd.DataFrame(index=np.arange(0, len(transect_names)),
-                                     columns=['time', 'transect', 'f_dswt', 'vel_dswt', 'transport_dswt', 'ds', 'dz_dswt', 'lon_transport', 'lat_transport', 'depth_transport'])
+                                     columns=['time', 'transect',
+                                              'f_dswt', 'vel', 'thickness', 'transport',
+                                              'distance', 'lon', 'lat', 'h',
+                                              'drhodx_mean', 'drhodx_min', 'drhos_mean', 'drhos_min'])
     time = pd.to_datetime(roms_ds.ocean_time.values[0]).date()
     row = 0
     for i, transect_name in enumerate(transect_names):
@@ -104,29 +117,30 @@ def determine_daily_dswt_along_multiple_transects(roms_ds:xr.Dataset, transects:
         xi = transects[transect_name]['xi']
         
         transect_ds = select_roms_transect_from_known_coordinates(roms_ds, eta, xi)
-        l_dswt, _, _, _, _ = determine_dswt_along_transect(transect_ds, config)
+        (t_dswt, vel, thickness, transport, distance, lon, lat, h, drhodx_mean, drhodx_min, drhos_mean, drhos_min) = determine_dswt_along_transect(transect_ds, config)
+        f_dswt = len(t_dswt) / len(transect_ds.ocean_time)
         
-        dswt_cross_transport, dswt_cross_vel, dswt_dz, ds, lon, lat, h = calculate_dswt_cross_shelf_transport_along_transect(transect_ds, l_dswt, config)
-        
-        for j in range(len(dswt_cross_transport)):
-            df_transects_dswt.loc[row] = [time, transect_name, np.nanmean(l_dswt.astype(int)),
-                                          dswt_cross_vel[j], dswt_cross_transport[j], ds[j], dswt_dz[j], lon[j], lat[j], h[j]]
+        for j in range(len(transport)):
+            df_transects_dswt.loc[row] = [time, transect_name, f_dswt,
+                                          vel[j], thickness[j], transport[j],
+                                          distance[j], lon[j], lat[j], h[j],
+                                          drhodx_mean[j], drhodx_min[j], drhos_mean[j], drhos_min[j]]
             row += 1
         
     return df_transects_dswt
 
 if __name__ == '__main__':
-    output_dswt = 'output/cwa_114-116E_33-31S/dswt_2017.csv'
+    output_dswt = 'output/test_114-116E_33-31S/dswt_2017.csv'
     
-    model_input_dir = get_dir_from_json('cwa')
-    files = ['cwa_20170420_03__his.nc']
+    model_input_dir = get_dir_from_json('test_data')
+    files = ['test_20170601_03__his.nc']
     grid_file = f'{model_input_dir}grid.nc'
     
     lon_range = [114., 116.]
     lat_range = [-33., -31.]
-    transects = read_transects_in_lon_lat_range_from_json('input/transects/cwa_transects.json', lon_range, lat_range)
+    transects = read_transects_in_lon_lat_range_from_json('input/transects/test_transects.json', lon_range, lat_range)
     
-    config = read_config('cwa')
+    config = read_config('test')
     
     for i in range(len(files)):
         roms_ds = load_roms_data(f'{model_input_dir}2017/{files[i]}', grid_file=grid_file)
